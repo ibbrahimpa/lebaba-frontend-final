ed1f38e0270b08f61a82484913abbd866a5e7207
ibbrahimpa <ebrahimpanvelwala@gmail.com>
2025-03-09 15:20:12 +0530

    url update

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
     <div className='section__container header__container'>
         <div className='header__content z-30'>
             <h4 className='uppercase'>UP TO 20% Discount on</h4>
-            <h1>Girl's Fashion</h1>
+            <h1>Boys's Fashion</h1>
             <p>Discover the latest trends and express your unique style with our Women's Fashion website. Explore a curated collection of clothing, accessories, and footwear that caters to every taste and occasion.</p>
             <button className='btn'><Link to='/shop'>EXPLORE NOW</Link></button>
         </div>
